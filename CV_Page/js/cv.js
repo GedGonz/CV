b8e8906a0f27cb4725c75a7f9b0ab9dd6f454bf9
Gerald Gonzalez Zeledon <gedgonz7@gmail.com>
2019-04-30 16:02:45 -0600

    Delete cv.js

--- CV_Page/js/cv.js
+++
@@ -1,7 +0,0 @@
-
-function calularEdad() {
-    var anioNacimiento=1991;
-    var anioActual=new Date().getFullYear();
-    var edad=(anioActual-anioNacimiento).toString();
-    $("#edad").text(edad+" años");
-}

c3539b7a378ef778f81549f6adb5b8b4d769f0c9
GedGonz <gedgonz7@gmail.com>
2019-05-04 21:32:53 -0600

    Cambio Edad

--- js/cv.js
+++ js/cv.js
@@ -1,7 +1,30 @@
-
 function calularEdad() {
-    var anioNacimiento=1991;
-    var anioActual=new Date().getFullYear();
-    var edad=(anioActual-anioNacimiento).toString();
-    $("#edad").text(edad+" años");
+    var FechaString = "05/05/1991";
+
+    var FechaNacimiento = new Date(FechaString);
+    var FechaActual = new Date();
+    var anioedad = (FechaActual.getFullYear() - FechaNacimiento.getFullYear());
+
+
+
+
+    var edad = (anioedad).toString();
+    $("#edad").text(edad + " años");
+}
+
+function EsoYaPasoCumpleAnios(FechaNacimiento, FechaActual) {
+
+    var Es_o_Ya_Paso_CumpleaAnio = false;
+    var YaPasoMesCumpleaños = ((FechaActual.getMonth() + 1) >= (FechaNacimiento.getMonth() + 1));
+    var YaPasoDiaCumpelaños = (FechaActual.getDate() >= FechaNacimiento.getDate());
+
+    var EsMesCumpleaños = ((FechaActual.getMonth() + 1) == (FechaNacimiento.getMonth() + 1));
+
+    if (EsMesCumpleaños && !YaPasoDiaCumpelaños) {
+
+    } else if (!YaPasoMesCumpleaños) {
+
+    } else {
+        Es_o_Ya_Paso_CumpleaAnio = true;
+    }
 }

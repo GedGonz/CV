b03a9af04a538bb8e4774c72c844bf3819dfd747
GedGonz <gedgonz7@gmail.com>
2019-05-04 22:05:40 -0600

    Nueva Funcion para Calcular la edad.

--- js/cv.js
+++ js/cv.js
@@ -1,30 +1,29 @@
 function calularEdad() {
-    var FechaString = "05/05/1991";
+    var FechaString = "05/07/1991";
 
     var FechaNacimiento = new Date(FechaString);
     var FechaActual = new Date();
-    var anioedad = (FechaActual.getFullYear() - FechaNacimiento.getFullYear());
+    var anios = (FechaActual.getFullYear() - FechaNacimiento.getFullYear());
 
+    var Es_o_Ya_Paso_CumpleaAnio = EsoYaPasoCumpleAnios(FechaNacimiento, FechaActual);
 
+    var edad = (Es_o_Ya_Paso_CumpleaAnio) ? anios : (--anios);
 
-
-    var edad = (anioedad).toString();
+    edad = (edad).toString();
     $("#edad").text(edad + " años");
 }
 
 function EsoYaPasoCumpleAnios(FechaNacimiento, FechaActual) {
 
     var Es_o_Ya_Paso_CumpleaAnio = false;
-    var YaPasoMesCumpleaños = ((FechaActual.getMonth() + 1) >= (FechaNacimiento.getMonth() + 1));
+    var YaPasoMesCumpleaños = ((FechaActual.getMonth() + 1) > (FechaNacimiento.getMonth() + 1));
+    var EsMesCumpleaños = ((FechaActual.getMonth() + 1) == (FechaNacimiento.getMonth() + 1));
     var YaPasoDiaCumpelaños = (FechaActual.getDate() >= FechaNacimiento.getDate());
 
-    var EsMesCumpleaños = ((FechaActual.getMonth() + 1) == (FechaNacimiento.getMonth() + 1));
-
-    if (EsMesCumpleaños && !YaPasoDiaCumpelaños) {
-
-    } else if (!YaPasoMesCumpleaños) {
-
-    } else {
+    if (YaPasoMesCumpleaños) {
+        Es_o_Ya_Paso_CumpleaAnio = true;
+    } else if (EsMesCumpleaños && YaPasoDiaCumpelaños) {
         Es_o_Ya_Paso_CumpleaAnio = true;
     }
+    return Es_o_Ya_Paso_CumpleaAnio;
 }
